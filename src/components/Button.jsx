0950be0123109d3fdfb94b0da10a64cac7df0576
dharnnyel <ubahchuks91@gmail.com>
2024-11-17 19:49:10 +0100

    implement login page

---
+++ src/components/Button.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Button = ({ children, size }) => {
+	let buttonWidth =
+		size === 's' ? 'w-1/5' : size === 'm' ? 'w-1/2' : 'w-11/12';
+
+	return (
+		<button
+			className={`bg-blue text-white font-inter py-1.5 rounded-xl ${buttonWidth}`}
+		>
+			{children}
+		</button>
+	);
+};
+
+export default Button;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+	theme: {
+		extend: {
+			colors: {
+				blue: '#007AFF',
+			},
+			fontFamily: {
+				inter: ['Inter', 'sans-serif'],
+				inria: ['Inria Serif', 'sans-serif']
+			},
+			screens: {
+				'xs': '375px'
+			}
+		},
+	},
+	plugins: [],
+};
