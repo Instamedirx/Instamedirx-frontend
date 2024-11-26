daae1a2805f27e899d3421f3165833103dac888a
olagunju-oluwabukola <olagunjuoluwabukola2018@gmail.com>
2024-11-26 14:58:49 +0100

    first merge

--- src/features/auth/pages/Login.jsx
+++ src/features/auth/pages/Login.jsx
@@ -1,5 +1,4 @@
 import { Link } from 'react-router-dom';
-import { Link } from 'react-router-dom';
 import Logo from '../../../assets/logo.svg';
 import SideImage from '../../../assets/login/side_image.svg';
 
