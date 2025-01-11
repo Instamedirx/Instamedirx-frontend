f7c0c6e1e1ca6ee54c8d801a51203f1347aaaec3
dharnnyel <ubahchuks91@gmail.com>
2025-01-11 15:55:54 +0100

    create client dashbord structure

---
+++ src/features/dashboards/client/pages/layout.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ClientLayout = () => {
+  return (
+    <div>
+      {/* Top side */}
+      {/* TODO: SIDE BAR */}
+      {/* TODO: MAIN LAYOUT */}
+    </div>
+  )
+}
+
+export default ClientLayout
